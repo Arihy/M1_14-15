b65c3e3b783fa0a73f6f6c19f520efb9e606936f
Arihy <ynallem@gmail.com>
2014-10-08 14:06:57 +0200

    [IA] wip backtrack

--- workspace/TP1-IA-etud/src/Solver.java
+++ workspace/TP1-IA-etud/src/Solver.java
@@ -1,48 +1,79 @@
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import javax.swing.text.html.HTMLDocument;
+import java.util.*;
 
 
-public class Solver {
+public class Solver
+{
 
 		public CSP problem; // l'instance de CSP                 
 		private HashMap<String,Object> assignation; // codage d'une solution partielle ou totale
 		
-		public Solver(CSP p) {
+		public Solver(CSP p)
+        {
 			problem = p;
 			assignation = new HashMap<String,Object>();               
 		}
 		
 		// retourne une solution s'il en existe une, null sinon           
-		public HashMap<String,Object> searchSolution() { 
-			// TO DO : appel de backtrack
-			return null;
+		public HashMap<String,Object> searchSolution()
+        {
+            assignation = new HashMap<String,Object>();
+			return  backtrack();
 		}
 		
-		// l'algo rŽcursif
-		private HashMap<String,Object> backtrack() {
-			// TO DO
+		// l'algo rï¿½cursif
+		private HashMap<String,Object> backtrack()
+        {
+			if(assignation.size() == problem.getVarNumber())
+            {
+                return assignation;
+            }
+            String x = chooseVar(problem.getVar(), assignation.keySet());
+            for(Iterator v = tri(problem.getDom(x)).iterator(); v.hasNext();)
+            {
+                HashMap<String,Object> newAssignation = assignation;
+                newAssignation.put(x, v.next());
+                if(consistant(newAssignation.keySet(), problem.getConstraints()))
+                {
+                    assignation = newAssignation;
+                    if(backtrack() != null)
+                    {
+                        return assignation;
+                    }
+                }
+            }
 			return null;   
 		}
 		 
 		// choix d'une variable
-		private String chooseVar(Set<String> allVar, Set<String> assignedVar) {
-			// TO DO
+		private String chooseVar(Set<String> allVar, Set<String> assignedVar)
+        {
+			for(String s : allVar)
+            {
+                if(!assignedVar.contains(s))
+                {
+                    return s;
+                }
+            }
 			return null;
 		}
 		
 		
-		private TreeSet<Object> tri(TreeSet<Object> values) {
-			// TO DO
-			return null;
+		private TreeSet<Object> tri(TreeSet<Object> values)
+        {
+			return values;
 		}
-		
+
+        private boolean consistant(Set<String> assignedVar, ArrayList<Constraint> constraints)
+        {
+            // TO DO
+            return false;
+        }
 		
 		// retourne l'ensemble des solutions
-		public HashSet<HashMap<String,Object>> searchAllSolutions() {
+		public HashSet<HashMap<String,Object>> searchAllSolutions()
+        {
 			// TO DO              
 			return null;
 		}
