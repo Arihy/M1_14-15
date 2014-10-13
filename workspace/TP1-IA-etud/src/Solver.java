5a09756c8c9ef58611e6d352761c1317f6dd224f
Arihy <ynallem@gmail.com>
2014-10-13 19:51:15 +0200

    [AI] solver v1 done

--- workspace/TP1-IA-etud/src/Solver.java
+++ workspace/TP1-IA-etud/src/Solver.java
@@ -5,109 +5,105 @@ import java.util.*;
 
 public class Solver
 {
+    public CSP problem; // l'instance de CSP
 
-		public CSP problem; // l'instance de CSP                 
-		private HashMap<String,Object> assignation; // codage d'une solution partielle ou totale
-		
-		public Solver(CSP p)
-        {
-			problem = p;
-			assignation = new HashMap<String,Object>();               
-		}
-		
-		// retourne une solution s'il en existe une, null sinon           
-		public HashMap<String,Object> searchSolution()
-        {
-            assignation = new HashMap<String,Object>();
-			return  backtrack();
-		}
-		
-		// l'algo r�cursif
-		private HashMap<String,Object> backtrack()
-        {
-			if(assignation.size() == problem.getVarNumber())
-            {
-                return assignation;
-            }
-            String x = chooseVar(problem.getVar(), assignation.keySet());
-            for(Iterator v = tri(problem.getDom(x)).iterator(); v.hasNext();)
-            {
-                HashMap<String,Object> newAssignation = new HashMap<String, Object>(assignation);
-                Object val = v.next() ;
-                newAssignation.put(x, val);
-                System.out.println("var : " + x + " val : " + val );
-                if(consistant(newAssignation, problem.getConstraints()))
-                {
-                    assignation = new HashMap<String, Object>(newAssignation);
-                    if(backtrack() != null)
-                    {
-                        return assignation;
-                    }
-                }
-            }
-			return null;   
-		}
-		 
-		// choix d'une variable
-		private String chooseVar(Set<String> allVar, Set<String> assignedVar)
-        {
-			for(String s : allVar)
-            {
-                if(!assignedVar.contains(s))
-                {
-                    return s;
-                }
-            }
-			return null;
-		}
-		
-		
-		private TreeSet<Object> tri(TreeSet<Object> values)
-        {
-			return values;
-		}
+    public Solver(CSP p)
+    {
+        problem = p;
+    }
 
-        private boolean consistant(HashMap<String,Object> assignedVar, ArrayList<Constraint> constraints)
+    // retourne une solution s'il en existe une, null sinon
+    public HashMap<String,Object> searchSolution()
+    {
+        HashMap<String,Object> assignation = new HashMap<String,Object>();
+        return  backtrack(assignation);
+    }
+
+    // l'algo r�cursif
+    private HashMap<String,Object> backtrack(HashMap<String,Object> assignation)
+    {
+        if(assignation.size() == problem.getVarNumber())
         {
-            boolean consistant = true;
-            for(Constraint c : constraints)
-            {
-                if(assignedVar.size() >= c.getArity())
-                {
-                    //tester si les variables assignées apparessent dans la constrainte
-                    if(assignedVar.keySet().containsAll(c.getVariables()))
-                    {
-                        //tester qu'il existe un tuple qui correspond aux variables assignées
-                        ArrayList<String> orderedVars = c.getVariables();
-                        boolean okTuple = false;
-                        int cptTuples = 0;
-                        Object[] tuples = (Object[])c.getTuples().toArray();
-                        while(cptTuples < tuples.length && !okTuple)
-                        {
-                            ArrayList tuple = (ArrayList)tuples[cptTuples];
-                            int i = 0;
-                            while(i<tuple.size() && assignedVar.get(orderedVars.get(i)).equals(tuple.get(i)))
-                            {
-                                i++;
-                            }
-                            if(i == tuple.size())
-                            {
-                                okTuple = true;
-                            }
-                            cptTuples ++;
-                        }
-                        if(!okTuple) return false;
-                        //consistant = false;
-                    }
-                }
-            }
-            return consistant;
+            return assignation;
         }
-		
-		// retourne l'ensemble des solutions
-		public HashSet<HashMap<String,Object>> searchAllSolutions()
+
+        String x = chooseVar(problem.getVar(), assignation.keySet());
+        for(Iterator v = tri(problem.getDom(x)).iterator(); v.hasNext();)
         {
-			// TO DO              
-			return null;
-		}
+            HashMap<String,Object> newAssignation = new HashMap<String, Object>(assignation);
+            Object val = v.next() ;
+            newAssignation.put(x, val);
+
+            if(consistant(newAssignation, problem.getConstraints()))
+            {
+                if(backtrack(newAssignation) != null)
+                {
+                    return backtrack(newAssignation);
+                }
+            }
+        }
+        return null;
+    }
+
+    // choix d'une variable
+    private String chooseVar(Set<String> allVar, Set<String> assignedVar)
+    {
+        for(String s : allVar)
+        {
+            if(!assignedVar.contains(s))
+            {
+                return s;
+            }
+        }
+        return null;
+    }
+
+
+    private TreeSet<Object> tri(TreeSet<Object> values)
+    {
+        return values;
+    }
+
+    // retourne l'ensemble des solutions
+    public HashSet<HashMap<String,Object>> searchAllSolutions()
+    {
+        // TO DO
+        return null;
+    }
+
+    private boolean consistant(HashMap<String,Object> assignedVar, ArrayList<Constraint> constraints)
+    {
+        boolean consistant = true;
+        for(Constraint c : constraints)
+        {
+            if(assignedVar.size() >= c.getArity())
+            {
+                //tester si les variables assignées apparessent dans la constrainte
+                if(assignedVar.keySet().containsAll(c.getVariables()))
+                {
+                    //tester qu'il existe un tuple qui correspond aux variables assignées
+                    ArrayList<String> orderedVars = c.getVariables();
+                    boolean okTuple = false;
+                    //créer un tuple avec les variables des assignations
+                    ArrayList<Object> valTuple = new ArrayList<Object>(orderedVars.size());
+                    for(int k = 0; k < orderedVars.size(); k++)
+                    {
+                        valTuple.add(k, assignedVar.get(orderedVars.get(k)));
+                    }
+
+                    //tester si le tuple crée appartient a la contrainte
+                    //si oui continuer avec la prochaine contrainte,
+                    //sinon echec
+                    if(c.getTuples().contains(valTuple))
+                    {
+                        okTuple = true;
+                        consistant = true;
+                    }
+                    if(!okTuple)
+                        return false;
+                }
+            }
+        }
+        return consistant;
+    }
 }
